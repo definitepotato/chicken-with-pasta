d686b25681cee19831f941eed2686d9700903a89
Ari Mizrahi <arianymizrahi@gmail.com>
2020-05-13 01:09:25 -0400

    Sloppy, but it's working.

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # chicken-with-pasta
 A project for New York Blockchain Week
+
+This code is work in progress, it's sloppy, some of it hardcoded and reads like crap.
+We are fleshing out an idea, any and all criticism welcome, but I can't promise you will like the feedback.
---
+++ bot.go
@@ -0,0 +1,93 @@
+package main
+
+import (
+  "math/rand"
+  "strconv"
+  "strings"
+  "flag"
+  "fmt"
+  "os"
+
+  "github.com/keybase/go-keybase-chat-bot/kbchat"
+  qrcode "github.com/skip2/go-qrcode"
+)
+
+// Function to gracefully fail and provide user with a useful message.
+func fail(msg string, args ...interface{}) {
+  fmt.Fprintf(os.Stderr, msg+"\n", args...)
+  os.Exit(3)
+}
+
+func main() {
+  var kbLoc string
+  var kbc *kbchat.API
+  var err error
+
+  flag.StringVar(&kbLoc, "keybase", "keybase", "the location of the Keybase app")
+  flag.Parse()
+
+  // Initiate keybase and establish API connection.
+  if kbc, err = kbchat.Start(kbchat.RunOptions{KeybaseLocation: kbLoc}); err != nil {
+    fail("Error creating API: %s", err.Error())
+  }
+
+  sub, err := kbc.ListenForNewTextMessages()
+  if err != nil {
+      fail("Error listening: %s", err.Error())
+  }
+
+  // Hardcoded map of Asset Code to Issuer Address.
+  var assetCode map[string]string
+  assetCode = make(map[string]string)
+  assetCode["USD"] = "GDUKMGUGDZQK6YHYA5Z6AY2G4XDSZPSZ3SW5UN3ARVMO6QSRDWP5YLEX"
+  assetCode["BTC"] = "GAUTUYY2THLF7SGITDFMXJVYH3LHDSMGEAKSBU267M2K7A3W543CKUEF"
+  assetCode["ETH"] = "GBDEVU63Y6NTHJQQZIKVTC23NWLQVP3WJ2RI2OTSJTNYOIGICST6DUXR"
+
+  // Begin loop to listen for messages in the comm channel.
+  for {
+    msg, err := sub.Read()
+    if err != nil{
+      fail("Failed to read message: %s", err.Error())
+    }
+
+    // Split received message by spaces into elements in a slice.
+    s := strings.Split(msg.Message.Content.Text.Body, " ")
+
+    // Ignore message unless it begins with !pay.
+    if s[0] == "!pay" {
+
+      // TODO: Validate Stellar Address.
+      // TODO: Validate Asset Code.
+      // TODO: Map Asset Code to default list.
+      // TODO: User specified Asset Issuer (end of message).
+      // TODO: Validate Asset Issuer if provided.
+      // TODO: Generate QR code.
+
+      if msg.Message.Content.TypeName != "text" {
+        continue
+      }
+
+      if msg.Message.Sender.Username == kbc.GetUsername() {
+        continue
+      }
+
+      // Check that we have a hardcoded Issuer for the provided Asset Code. This is sloppy shit, we can do better.
+      if ac, ok := assetCode[s[3]]; ok {
+        // Build Stellar Pay URL.
+        spurl := "web+stellar:pay?destination=" + s[1] + "&amount=" + s[2] + "&asset_code=" + s[3] + "&asset_issuer=" + ac
+
+        // Build QR Code. More hardcoded shit, need to scrap this later.
+        qrName := strconv.Itoa(rand.Int()) + ".png"
+        fileLoc := "/var/www/html/" + qrName
+        payMsg := "Pay at: https://chickenwithpasta.com/" + qrName
+        err := qrcode.WriteFile(spurl, qrcode.Medium, 256, qrName)
+        err = os.Rename(qrName, fileLoc)
+
+        if _, err = kbc.SendMessage(msg.Message.Channel, payMsg); err != nil {
+          fail("Error echo'ing message: %s", err.Error())
+        }
+      }
+    }
+
+  }
+}
